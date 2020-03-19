d6d889d61bb45feda591afcad31a6de2325c888b
Username0720 <39237166+Username0720@users.noreply.github.com>
2020-03-19 12:51:41 +0300

    2forms

--- WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/Form2.cs
@@ -12,8 +12,8 @@ namespace WindowsFormsApp1
 {
     public partial class Form2 : Form
     {
-        Form3 f3 = new Form3();
-        Form2 f2 = new Form2();
+        //Form3 f3 = new Form3();
+        //Form2 f2 = new Form2();
         public Form2()
         {
             InitializeComponent();
@@ -27,29 +27,29 @@ namespace WindowsFormsApp1
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            f3.Show();
+            Show(new Form3());
         }
 
 
 
         private void RadioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            pictureBox1 = Image.FromFile(Properties.Resources._113264_krasota_rastsvet_tsvetok_sad_roz_uhod_za_licom_3840x2160);
+            //pictureBox1 = Image.FromFile(Properties.Resources._113264_krasota_rastsvet_tsvetok_sad_roz_uhod_za_licom_3840x2160);
     }
 
         private void Button1_MouseMove(object sender, MouseEventArgs e)
         {
-            f2.BackgroundImage = pictureBox1.Image;
+            BackgroundImage = pictureBox1.Image;
         }
 
         private void RadioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            pictureBox1.Image = Image.FromFile(Properties.Resources._113523_nebo_vihr_lazurnyj_prostranstvo_kosmos_2560x1600);
+            //pictureBox1.Image = Image.FromFile(Properties.Resources._113523_nebo_vihr_lazurnyj_prostranstvo_kosmos_2560x1600);
         }
 
         private void RadioButton3_CheckedChanged(object sender, EventArgs e)
         {
-            pictureBox1.Image = Image.FromFile(Properties.Resources._18e576a5623628b6f4975c09a2e39921);
+            //pictureBox1.Image = Image.FromFile(Properties.Resources._18e576a5623628b6f4975c09a2e39921);
         }
     }
 }
