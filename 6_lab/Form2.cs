c30058f0e9b2a4c8dd7b81e6552e45dfc9cd9428
Username0720 <39237166+Username0720@users.noreply.github.com>
2020-03-26 16:26:07 +0300

    6 laba

---
+++ 6_lab/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _6_lab
+{
+    public partial class Form2 : Form
+    {
+        public int line;
+        public User user;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Visible = false;
+            //if (textBox1.Text == textBox2.Text) button1.Visible = true;
+            user.ChangePassword(textBox1.Text, line);
+            this.Close();
+        }
+    }
+}
