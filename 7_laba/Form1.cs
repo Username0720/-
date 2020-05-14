8e24adf532096172780a7c178252b45fd6afcbc7
User <39237166+Username0720@users.noreply.github.com>
2020-05-14 21:54:01 +0300

    Commited lab№7 & changed lab№9

---
+++ 7_laba/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7_laba
+{
+    public partial class Form1 : Form
+    {
+        public demoEntities1 db = new demoEntities1();
+        public List<students> studentsheet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            studentsheet = (from stud in db.students
+                            select stud).ToList();
+
+            var query = (from stud in studentsheet
+                         join g in db.groups on stud.code_group equals g.code_group
+                         orderby stud.code_stud
+                         select new { stud.code_stud, stud.surname, stud.name, g.name_group }).ToList();
+
+            dataGridView1.DataSource = query;
+            dataGridView1.ReadOnly = true;
+            if (dataGridView1.RowCount == 0) label1.Visible = true;
+            else label1.Visible = false;
+
+            dataGridView1.Columns[0].HeaderText = "Номер студента";
+            dataGridView1.Columns[1].HeaderText = "Фамилия";
+            dataGridView1.Columns[2].HeaderText = "Имя";
+            dataGridView1.Columns[3].HeaderText = "Номер группы";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var query = (from stud in studentsheet
+                         join g in db.groups on stud.code_group equals g.code_group
+                         orderby stud.code_stud
+                         select new { stud.code_stud, stud.surname, stud.name, g.name_group }).ToList();
+
+            if (textBox1.Text != "")
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        dataGridView1.DataSource = query.Where(p => p.code_stud.ToString() == textBox1.Text.ToString()).ToList();
+                        break;
+                    case 1:
+                        dataGridView1.DataSource = query.Where(p => p.surname.ToString() == textBox1.Text.ToString()).ToList();
+                        break;
+                    case 2:
+                        dataGridView1.DataSource = query.Where(p => p.name.ToString() == textBox1.Text.ToString()).ToList();
+                        break;
+                    case 3:
+                        dataGridView1.DataSource = query.Where(p => p.name_group.ToString() == textBox1.Text.ToString()).ToList();
+                        break;
+                }
+            }
+            else dataGridView1.DataSource = query;
+
+            dataGridView1.Update();
+
+            if (dataGridView1.RowCount == 0) label1.Visible = true;
+            else label1.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Application.OpenForms.Count == 1)
+            {
+                FormAddStudent addSt = new FormAddStudent();
+                addSt.Owner = this;
+                addSt.Show();
+            }
+            else Application.OpenForms[0].Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<students> query = (from stud in db.students
+                                    select stud).ToList();
+
+            if(dataGridView1.SelectedCells.Count == 1)
+            {
+                if (Application.OpenForms.Count == 1)
+                {
+                    students item = query.First(w => w.code_stud.ToString() == dataGridView1.SelectedCells[0].OwningRow.Cells[0].Value.ToString());
+
+                    FormEditStudent editSt = new FormEditStudent(item);
+                    editSt.Owner = this;
+                    editSt.Show();
+                }
+                else Application.OpenForms[0].Focus();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var query = (from stud in studentsheet
+                         join g in db.groups on stud.code_group equals g.code_group
+                         orderby stud.code_stud
+                         select new { stud.code_stud, stud.surname, stud.name, g.name_group });
+
+            dataGridView1.DataSource = query;
+            dataGridView1.Columns[0].HeaderText = "Номер студента";
+            dataGridView1.Columns[1].HeaderText = "Фамилия";
+            dataGridView1.Columns[2].HeaderText = "Имя";
+            dataGridView1.Columns[3].HeaderText = "Номер группы";
+            button1_Click(sender, e);
+        }
+    }
+}
